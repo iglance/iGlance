4905b2b285fb2b1c31ea80d3a14874a610bf95f7
Cemal <cemal@MacBook-Pro.local>
2018-06-01 00:34:41 +0200

    first working hello world

---
+++ iGlance/iGlance/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iGlance
+//
+//  Created by Cemal on 01.06.18.
+//  Copyright © 2018 iGlance Corp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iGlance/iGlance/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iGlance
+//
+//  Created by Cemal on 01.06.18.
+//  Copyright © 2018 iGlance Corp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
