0e2badf5c047c2117285230af54b1dca069c2818
Cemal <cemal@MacBook-Pro.local>
2018-06-01 03:44:18 +0200

    mouseDown event works on cpu now, pb rendering also, missing NSTask to set pb percentage correctly

--- iGlance/iGlance/CPUUsageView.swift
+++ iGlance/iGlance/CPUUsageView.swift
@@ -11,44 +11,44 @@ import Cocoa
 class CPUUsageView: NSView {
 
     var pbFillRect: NSRect?
-    var myrect2: NSRect?
     var mouseIsDown: Bool?
+    var pbMax: Double?
+    var cMouseDown: NSColor?
+    var pixelWidth: Double?
+    var pixelHeight: Double?
     
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         
         if (mouseIsDown == true)
         {
-            let c1  = NSColor(calibratedRed: 48.0/255.0, green: 91.0/255.0, blue: 178.0/255.0, alpha: 1.0)
-            c1.setFill()
+            cMouseDown?.setFill()
             self.frame.fill()
             NSColor.clear.setFill()
-            
-            let img1 = NSImage(named:NSImage.Name("menubar-label-cpu-white"))
-            img1?.draw(at: NSPoint(x: 2, y: 3), from: self.frame, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-            let img2 = NSImage(named:NSImage.Name("progressbar-white"))
-            img2?.draw(at: NSPoint(x: 12, y: 3), from: self.frame, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-            pbFillRect = NSRect(x: 13, y: 4, width: 7, height: 16)
-            NSColor.red.setFill()
-            pbFillRect?.fill()
-            NSColor.clear.setFill()
         }
-        else
-        {
-            let img1 = NSImage(named:NSImage.Name("menubar-label-cpu-white"))
-            img1?.draw(at: NSPoint(x: 2, y: 3), from: self.frame, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-            let img2 = NSImage(named:NSImage.Name("progressbar-white"))
-            img2?.draw(at: NSPoint(x: 12, y: 3), from: self.frame, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
-            pbFillRect = NSRect(x: 13, y: 4, width: 7, height: 16)
-            NSColor.red.setFill()
-            pbFillRect?.fill()
-            NSColor.clear.setFill()
-        }
+        let img1 = NSImage(named:NSImage.Name("menubar-label-cpu-white"))
+        img1?.draw(at: NSPoint(x: 2, y: 3), from: self.frame, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
+        let img2 = NSImage(named:NSImage.Name("progressbar-white"))
+        img2?.draw(at: NSPoint(x: 12, y: 3), from: self.frame, operation: NSCompositingOperation.sourceOver, fraction: 1.0)
+        pbFillRect = NSRect(x: 13.0, y: 4.0, width: pixelWidth!, height: pixelHeight!)
+        NSColor.red.setFill()
+        pbFillRect?.fill()
+        NSColor.clear.setFill()
+    }
+    
+    func setPercent(percent: Double)
+    {
+        pixelHeight = Double((pbMax! / 100.0) * percent)
+        needsDisplay = true
     }
     
     override init(frame frameRect: NSRect)
     {
         super.init(frame: frameRect)
+        cMouseDown = NSColor(calibratedRed: 48.0/255.0, green: 91.0/255.0, blue: 178.0/255.0, alpha: 1.0)
+        pbMax = 16.0 // 32*0.5
+        pixelWidth = 7 // 14*0.5
+        pixelHeight = 0
     }
     
     required init?(coder decoder: NSCoder) {
