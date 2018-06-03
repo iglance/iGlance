91a7b497f6d89c39ae3a297d1de299c9465e66b5
Cemal Kaygusuz <cemalkaygusuz97@gmail.com>
2018-06-03 06:31:09 +0200

    CPU Usage works

---
+++ iGlance/Carthage/Checkouts/SystemKit/SystemKit/Battery.swift
@@ -0,0 +1,362 @@
+//
+// Battery.swift
+// SystemKit
+//
+// The MIT License
+//
+// Copyright (C) 2014, 2015  beltex <https://github.com/beltex>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import IOKit
+import Foundation
+
+/**
+Battery statistics for OS X, read-only.
+
+http://www.apple.com/batteries/
+
+TODO: None of this will work for iOS as I/O Kit is a private framework there
+*/
+public struct Battery {
+    
+    //--------------------------------------------------------------------------
+    // MARK: PUBLIC ENUMS
+    //--------------------------------------------------------------------------
+    
+    
+    /// Temperature units
+    public enum TemperatureUnit {
+        case Celsius
+        case Fahrenheit
+        case Kelvin
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    // MARK: PRIVATE ENUMS
+    //--------------------------------------------------------------------------
+    
+    
+    /// Battery property keys. Sourced via 'ioreg -brc AppleSmartBattery'
+    private enum Key: String {
+        case ACPowered        = "ExternalConnected"
+        case Amperage         = "Amperage"
+        /// Current charge
+        case CurrentCapacity  = "CurrentCapacity"
+        case CycleCount       = "CycleCount"
+        /// Originally DesignCapacity == MaxCapacity
+        case DesignCapacity   = "DesignCapacity"
+        case DesignCycleCount = "DesignCycleCount9C"
+        case FullyCharged     = "FullyCharged"
+        case IsCharging       = "IsCharging"
+        /// Current max charge (this degrades over time)
+        case MaxCapacity      = "MaxCapacity"
+        case Temperature      = "Temperature"
+        /// Time remaining to charge/discharge
+        case TimeRemaining    = "TimeRemaining"
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    // MARK: PRIVATE PROPERTIES
+    //--------------------------------------------------------------------------
+    
+    
+    /// Name of the battery IOService as seen in the IORegistry
+    private static let IOSERVICE_BATTERY = "AppleSmartBattery"
+    
+    
+    private var service: io_service_t = 0
+    
+    
+    //--------------------------------------------------------------------------
+    // MARK: PUBLIC INITIALIZERS
+    //--------------------------------------------------------------------------
+    
+    
+    public init() { }
+    
+    
+    //--------------------------------------------------------------------------
+    // MARK: PUBLIC METHODS
+    //--------------------------------------------------------------------------
+    
+    
+    /**
+    Open a connection to the battery.
+    
+    :returns: kIOReturnSuccess on success.
+    */
+    public mutating func open() -> kern_return_t {
+        if (service != 0) {
+            #if DEBUG
+                println("WARNING - \(__FILE__):\(__FUNCTION__) - " +
+                        "\(Battery.IOSERVICE_BATTERY) connection already open")
+            #endif
+            return kIOReturnStillOpen
+        }
+        
+        
+        // TODO: Could there be more than one service? serveral batteries?
+        service = IOServiceGetMatchingService(kIOMasterPortDefault,
+         IOServiceNameMatching(Battery.IOSERVICE_BATTERY).takeUnretainedValue())
+        
+        if (service == 0) {
+            #if DEBUG
+                println("ERROR - \(__FILE__):\(__FUNCTION__) - " +
+                        "\(Battery.IOSERVICE_BATTERY) service not found")
+            #endif
+            return kIOReturnNotFound
+        }
+        
+        return kIOReturnSuccess
+    }
+    
+    
+    /**
+    Close the connection to the battery.
+    
+    :returns: kIOReturnSuccess on success.
+    */
+    public mutating func close() -> kern_return_t {
+        let result = IOObjectRelease(service)
+        service    = 0     // Reset this incase open() is called again
+        
+        #if DEBUG
+            if (result != kIOReturnSuccess) {
+                println("ERROR - \(__FILE__):\(__FUNCTION__) - Failed to close")
+            }
+        #endif
+        
+        return result
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    // MARK: PUBLIC METHODS - BATTERY
+    //--------------------------------------------------------------------------
+    
+    
+    /**
+    Get the current capacity of the battery in mAh. This is essientally the
+    current charge of the battery.
+    
+    https://en.wikipedia.org/wiki/Ampere-hour
+    */
+    public func currentCapacity() -> Int {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.CurrentCapacity.rawValue,
+                                                   kCFAllocatorDefault,0)
+        return prop.takeUnretainedValue() as! Int
+    }
+    
+    
+    /**
+    Get the current max capacity of the battery in mAh. This degrades over time
+    from the original design capacity.
+    
+    https://en.wikipedia.org/wiki/Ampere-hour
+    */
+    public func maxCapactiy() -> Int {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.MaxCapacity.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Int
+    }
+    
+    
+    /**
+    Get the designed capacity of the battery in mAh. This is a static value.
+    The max capacity will be equal to this when new, and as it degrades over
+    time, be less than this.
+    
+    https://en.wikipedia.org/wiki/Ampere-hour
+    */
+    public func designCapacity() -> Int {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.DesignCapacity.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Int
+    }
+    
+    
+    /**
+    Get the current cycle count of the battery.
+
+    https://en.wikipedia.org/wiki/Charge_cycle
+    */
+    public func cycleCount() -> Int {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.CycleCount.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Int
+    }
+    
+    
+    /**
+    Get the designed cycle count of the battery.
+    
+    https://en.wikipedia.org/wiki/Charge_cycle
+    */
+    public func designCycleCount() -> Int {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                  Key.DesignCycleCount.rawValue,
+                                                  kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Int
+    }
+    
+    
+    /**
+    Is the machine powered by AC? Plugged into the charger.
+    
+    :returns: True if it is, false otherwise.
+    */
+    public func isACPowered() -> Bool {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.ACPowered.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Bool
+    }
+    
+    
+    /**
+    Is the battery charging?
+    
+    :returns: True if it is, false otherwise.
+    */
+    public func isCharging() -> Bool {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.IsCharging.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Bool
+    }
+    
+    
+    /**
+    Is the battery fully charged?
+    
+    :returns: True if it is, false otherwise.
+    */
+    public func isCharged() -> Bool {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.FullyCharged.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Bool
+    }
+    
+    
+    /**
+    What is the current charge of the machine? As seen in the battery status 
+    menu bar. This is the currentCapacity / maxCapacity.
+    
+    :returns: The current charge as a % out of 100.
+    */
+    public func charge() -> Double {
+        return floor(Double(currentCapacity()) / Double(maxCapactiy()) * 100.0)
+    }
+    
+    
+    /**
+    The time remaining to full charge (if plugged into AC), or the time
+    remaining to full discharge (running on battery). See also
+    timeRemainingFormatted().
+    
+    :returns: Time remaining in minutes.
+    */
+    public func timeRemaining() -> Int {        
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.TimeRemaining.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        return prop.takeUnretainedValue() as! Int
+    }
+
+    
+    /**
+    Same as timeRemaining(), but returns as a human readable time format, as
+    seen in the battery status menu bar.
+    
+    :returns: Time remaining string in the format <HOURS>:<MINUTES>
+    */
+    public func timeRemainingFormatted() -> String {
+        let time = timeRemaining()
+        return NSString(format: "%d:%02d", time / 60, time % 60) as String
+    }
+    
+    
+    /**
+    Get the current temperature of the battery.
+    
+    "MacBook works best at 50° to 95° F (10° to 35° C). Storage temperature: 
+     -4° to 113° F (-20° to 45° C)." - via Apple
+    
+    http://www.apple.com/batteries/maximizing-performance/
+    
+    :returns: Battery temperature, by default in Celsius.
+    */
+    public func temperature(unit: TemperatureUnit = .Celsius) -> Double {
+        let prop = IORegistryEntryCreateCFProperty(service,
+                                                   Key.Temperature.rawValue,
+                                                   kCFAllocatorDefault, 0)
+        
+        var temperature = prop.takeUnretainedValue() as! Double / 100.0
+        
+        switch unit {
+            case .Celsius:
+                // Do nothing - in Celsius by default
+                // Must have complete switch though with executed command
+                break
+            case .Fahrenheit:
+                temperature = Battery.toFahrenheit(temperature)
+            case .Kelvin:
+                temperature = Battery.toKelvin(temperature)
+        }
+        
+        return ceil(temperature)
+    }
+    
+    
+    //--------------------------------------------------------------------------
+    // MARK: PRIVATE HELPERS
+    //--------------------------------------------------------------------------
+    
+    
+    /**
+    Celsius to Fahrenheit
+    
+    :param: temperature Temperature in Celsius
+    :returns: Temperature in Fahrenheit
+    */
+    private static func toFahrenheit(temperature: Double) -> Double {
+        // https://en.wikipedia.org/wiki/Fahrenheit#Definition_and_conversions
+        return (temperature * 1.8) + 32
+    }
+    
+    
+    /**
+    Celsius to Kelvin
+    
+    :param: temperature Temperature in Celsius
+    :returns: Temperature in Kelvin
+    */
+    private static func toKelvin(temperature: Double) -> Double {
+        // https://en.wikipedia.org/wiki/Kelvin
+        return temperature + 273.15
+    }
+}
---
+++ iGlance/Carthage/Checkouts/SystemKit/SystemKit/Process.swift
@@ -0,0 +1,212 @@
+//
+// Process.swift
+// SystemKit
+//
+// The MIT License
+//
+// Copyright (C) 2014, 2015  beltex <https://github.com/beltex>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Darwin
+import Foundation
+
+//------------------------------------------------------------------------------
+// MARK: GLOBAL PUBLIC PROPERTIES
+//------------------------------------------------------------------------------
+
+// As defined in <mach/machine.h>
+
+/// Assuming this is interpreted as unknown for now
+public let CPU_TYPE_ANY       : cpu_type_t = -1
+public let CPU_TYPE_X86       : cpu_type_t = 7
+public let CPU_TYPE_I386      : cpu_type_t = CPU_TYPE_X86   // For compatibility
+public let CPU_TYPE_X86_64    : cpu_type_t = CPU_TYPE_X86 | CPU_ARCH_ABI64
+public let CPU_TYPE_ARM       : cpu_type_t = 12
+public let CPU_TYPE_ARM64     : cpu_type_t = CPU_TYPE_ARM | CPU_ARCH_ABI64
+public let CPU_TYPE_POWERPC   : cpu_type_t = 18
+public let CPU_TYPE_POWERPC64 : cpu_type_t = CPU_TYPE_POWERPC | CPU_ARCH_ABI64
+
+//------------------------------------------------------------------------------
+// MARK: PUBLIC STRUCTS
+//------------------------------------------------------------------------------
+
+/// Process information
+public struct ProcessInfo {
+    let pid     : Int
+    let ppid    : Int
+    let pgid    : Int
+    let uid     : Int
+    let command : String
+    /// What architecture was this process compiled for?
+    let arch    : cpu_type_t
+    /// sys/proc.h - SIDL, SRUN, SSLEEP, SSTOP, SZOMB
+    var status  : Int32
+}
+
+/// Process API
+public struct ProcessAPI {
+    
+    //--------------------------------------------------------------------------
+    // MARK: PRIVATE PROPERTIES
+    //--------------------------------------------------------------------------
+
+    private static let machHost = mach_host_self()
+    
+    //--------------------------------------------------------------------------
+    // MARK: PUBLIC INITIALIZERS
+    //--------------------------------------------------------------------------
+    
+    public init() { }
+    
+    //--------------------------------------------------------------------------
+    // MARK: PUBLIC METHODS
+    //--------------------------------------------------------------------------
+    
+    /// Return list of currently running processes
+    public static func list() -> [ProcessInfo] {
+        var list                         = [ProcessInfo]()
+        var psets                        = processor_set_name_array_t.alloc(1)
+        var pcnt: mach_msg_type_number_t = 0
+
+        // Need root
+        var result = host_processor_sets(machHost, &psets, &pcnt)
+        if result != KERN_SUCCESS {
+            #if DEBUG
+                println("ERROR - \(__FILE__):\(__FUNCTION__) - Need root - " +
+                        "kern_return_t: \(result)")
+            #endif
+            return list
+        }
+        
+        
+        // For each CPU set
+        for var i = 0; i < Int(pcnt); ++i {
+            var pset: processor_set_name_t = 0
+            result = host_processor_set_priv(machHost, psets[i], &pset)
+            
+            if result != KERN_SUCCESS {
+                #if DEBUG
+                    println("ERROR - \(__FILE__):\(__FUNCTION__) - CPU set " +
+                            "\(i) - kern_return_t: \(result)")
+                #endif
+                continue
+            }
+            
+            
+            // Get port to each task
+            var tasks                             = task_array_t.alloc(1)
+            var taskCount: mach_msg_type_number_t = 0
+            result = processor_set_tasks(pset, &tasks, &taskCount)
+            
+            if result != KERN_SUCCESS {
+                #if DEBUG
+                    println("ERROR - \(__FILE__):\(__FUNCTION__) - failed to "
+                            + " get tasks - kern_return_t: \(result)")
+                #endif
+                continue
+            }
+
+            
+            // For each task
+            for var x = 0; x < Int(taskCount); ++x {
+                let task       = tasks[x]
+                var pid: pid_t = 0
+                
+                pid_for_task(task, &pid)
+                
+                
+                // BSD layer only stuff
+                var kinfo = kinfo_proc()
+                var size  = strideof(kinfo_proc)
+                var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
+                
+                // TODO: Error check
+                sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0)
+
+                let command = withUnsafePointer(&kinfo.kp_proc.p_comm) {
+                    String.fromCString(UnsafePointer($0))!
+                }
+                
+                
+                list.append(ProcessInfo(pid    : Int(pid),
+                                        ppid   : Int(kinfo.kp_eproc.e_ppid),
+                                        pgid   : Int(kinfo.kp_eproc.e_pgid),
+                                        uid    : Int(kinfo.kp_eproc.e_ucred.cr_uid),
+                                        command: command,
+                                        arch   : arch(pid),
+                                        status : Int32(kinfo.kp_proc.p_stat)))
+                
+                mach_port_deallocate(mach_task_self_, task)
+            }
+            
+            // TODO: Missing deallocate for tasks
+            mach_port_deallocate(mach_task_self_, pset)
+            mach_port_deallocate(mach_task_self_, psets[i])
+            
+            // TODO: Why do dealloc calls on tasks and psets fail?
+        }
+        
+        return list
+    }
+
+    //--------------------------------------------------------------------------
+    // MARK: PRIVATE METHODS
+    //--------------------------------------------------------------------------
+    
+    /// What architecture was this process compiled for?
+    private static func arch(pid: pid_t) -> cpu_type_t {
+        var arch = CPU_TYPE_ANY
+        
+        // sysctl.proc_cputype not documented anywhere. Doesn't even show up
+        // when running 'sysctl -A'. Have to call sysctlnametomib() before hand
+        // due to this
+        // TODO: Call sysctlnametomib() only once
+        var mib       = [Int32](count: Int(CTL_MAXNAME), repeatedValue: 0)
+        var mibLength = size_t(CTL_MAXNAME)
+        
+        var result = sysctlnametomib("sysctl.proc_cputype", &mib, &mibLength)
+
+        if result != 0 {
+            #if DEBUG
+                println("ERROR - \(__FILE__):\(__FUNCTION__):\(__LINE__) - "
+                        + "\(result)")
+            #endif
+
+            return arch
+        }
+        
+        
+        mib[Int(mibLength)] = pid
+        var size = sizeof(cpu_type_t)
+
+        result = sysctl(&mib, u_int(mibLength + 1), &arch, &size, nil, 0)
+
+        if result != 0 {
+            #if DEBUG
+                println("ERROR - \(__FILE__):\(__FUNCTION__):\(__LINE__) - "
+                        + "\(result)")
+            #endif
+
+            arch = CPU_TYPE_ANY
+        }
+
+        return arch
+    }
+}
