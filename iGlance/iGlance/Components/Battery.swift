500367b386258152af5061e8eceb6d4fecffb8ac
Dominik <36657497+D0miH@users.noreply.github.com>
2018-06-13 18:15:31 +0200

    added swiftlint file and linted Battery.swift

--- iGlance/iGlance/Components/Battery.swift
+++ iGlance/iGlance/Components/Battery.swift
@@ -9,7 +9,7 @@ import Foundation
 import IOKit.ps
 
 class Battery {
-    
+
     /**
      * Battery variables
      */
@@ -27,7 +27,7 @@ class Battery {
         var hours: Int
         var minutes: Int
     }
-    
+
     init() {
         currentCapacity = getBatteryCapacity()
         previousChargeValue = currentCapacity
@@ -38,16 +38,16 @@ class Battery {
         currentCapacity = getBatteryCapacity()
         let lower = Double(AppDelegate.UserSettings.lowerBatteryNotificationValue)
         let upper = Double(AppDelegate.UserSettings.upperBatteryNotificationValue)
-        
-        if(previousChargeValue < upper && currentCapacity >= upper && alreadyNotified == false) {
+
+        if previousChargeValue < upper && currentCapacity >= upper && alreadyNotified == false {
             deliverBatteryNotification(message: "Battery is almost fully charged")
-        } else if(previousChargeValue > lower && currentCapacity <= lower && alreadyNotified == false) {
+        } else if previousChargeValue > lower && currentCapacity <= lower && alreadyNotified == false {
             deliverBatteryNotification(message: "Battery is low")
-        } else if(currentCapacity < upper && currentCapacity > lower){
+        } else if currentCapacity < upper && currentCapacity > lower {
             alreadyNotified = false
             NSUserNotificationCenter.default.removeAllDeliveredNotifications()
         }
-        
+
         previousChargeValue = currentCapacity
     }
 
@@ -61,15 +61,14 @@ class Battery {
         alreadyNotified = true
     }
 
-
     func getRemainingBatteryTime() -> RemainingBatteryTime {
         let remainingSeconds: CFTimeInterval = IOPSGetTimeRemainingEstimate()
-        if(remainingSeconds > 0.0) {
+        if remainingSeconds > 0.0 {
             let timeInMinutes = remainingSeconds/60
             let hours = Int(floor(timeInMinutes/60))
             let minutes = Int(timeInMinutes) % 60
             return RemainingBatteryTime(timeInSeconds: remainingSeconds, hours: hours, minutes: minutes)
-        } else if(remainingSeconds == -1.0) {
+        } else if remainingSeconds == -1.0 {
             return RemainingBatteryTime(timeInSeconds: remainingSeconds, hours: 0, minutes: 0)
         } else {
             return RemainingBatteryTime(timeInSeconds: remainingSeconds, hours: 0, minutes: 0)
@@ -79,16 +78,15 @@ class Battery {
     func getBatteryCapacity() -> Double {
         let snapshot = IOPSCopyPowerSourcesInfo().takeRetainedValue()
         let sources = IOPSCopyPowerSourcesList(snapshot).takeRetainedValue() as Array
-        for ps in sources {
-            let info = IOPSGetPowerSourceDescription(snapshot, ps).takeUnretainedValue() as! [String: AnyObject]
-            
-            let name = info[kIOPSNameKey] as? String
-            let currentCapacity = info[kIOPSCurrentCapacityKey] as? Int
-            if(name != nil && name == "InternalBattery-0" && currentCapacity != nil) {
-                return Double(currentCapacity!)
+        for powerSource in sources {
+            if let info = IOPSGetPowerSourceDescription(snapshot, powerSource).takeUnretainedValue() as? [String: AnyObject] {
+                let name = info[kIOPSNameKey] as? String
+                let currentCapacity = info[kIOPSCurrentCapacityKey] as? Int
+                if name != nil && name == "InternalBattery-0" && currentCapacity != nil {
+                    return Double(currentCapacity!)
+                }
             }
         }
         return -1.0
     }
 }
-
