079f7714de0991059a04f378901a5da3404d5545
Dominik <36657497+D0miH@users.noreply.github.com>
2019-02-28 09:13:05 +0100

    increased version

--- iGlance/iGlance/AppDelegate.swift
+++ iGlance/iGlance/AppDelegate.swift
@@ -53,7 +53,7 @@ extension NSColor {
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    public static var VERSION = "1.3.1"
+    public static var VERSION = "1.3.2"
 
     var myWindowController: MyMainWindow?
     
