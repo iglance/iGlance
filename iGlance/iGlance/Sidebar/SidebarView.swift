16ec812a3f3d23b4858f572fc33b4d12ff3cc5df
Dominik H <36657497+D0miH@users.noreply.github.com>
2019-12-17 17:39:14 +0100

    added custom sidebar theme colors

--- iGlance/iGlance/Sidebar/SidebarView.swift
+++ iGlance/iGlance/Sidebar/SidebarView.swift
@@ -14,7 +14,7 @@ class SidebarView: NSView {
         super.draw(dirtyRect)
         
         // set the background color of the sidebar
-        NSColor(red: 97/255, green: 97/255, blue: 97/255, alpha: 1).setFill()
+        ThemeManager.currentTheme().sidebarColor.setFill()
         dirtyRect.fill()
     }
 }
---
+++ iGlance/iGlance/Theme/ThemeManager.swift
@@ -0,0 +1,85 @@
+//
+//  ThemeManager.swift
+//  iGlance
+//
+//  Created by Dominik on 17.12.19.
+//  Copyright © 2019 D0miH. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+extension NSColor {
+    /**
+     * Returns the corresponding NSColor to the given hex color string.
+     *
+     * - Parameter hex: The given hex string
+     * - Parameter alpha: The alpha value of the color. This value should be between 0.0 and 1.0
+     */
+    static func colorFrom(hex: String, alpha: Float? = nil) -> NSColor {
+        var colorString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        // remove the hastag if present
+        if colorString.hasPrefix("#") {
+            colorString.remove(at: colorString.startIndex)
+        }
+        
+        // if the character count is not correct return the default color gray
+        if colorString.count != 6 {
+            return NSColor.blue
+        }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: colorString).scanHexInt64(&rgbValue)
+        
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+        
+        return NSColor (
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: CGFloat(alpha ?? 1.0)
+        )
+    }
+}
+
+enum Theme: Int {
+    case darkTheme, lightTheme
+    
+    var sidebarColor: NSColor {
+        switch self {
+            case .darkTheme:
+                return NSColor.colorFrom(hex: "#303440")
+            case .lightTheme:
+                return NSColor.colorFrom(hex: "#D9D9D9")
+        }
+    }
+    
+    var sidebarFontColor: NSColor {
+        switch self {
+            case .darkTheme:
+                return NSColor.colorFrom(hex: "#D9D9D9")
+        case .lightTheme:
+            return NSColor.colorFrom(hex: "#5069B3")
+        }
+    }
+    
+    
+}
+
+class ThemeManager {
+    
+    private static func isDarkTheme() -> Bool {
+        return UserDefaults.standard.string(forKey: "AppleInterfaceStyle") != nil
+    }
+    
+    static func currentTheme() -> Theme {
+        if isDarkTheme() {
+            return Theme.darkTheme
+        } else {
+            return Theme.lightTheme
+        }
+    }
+}
