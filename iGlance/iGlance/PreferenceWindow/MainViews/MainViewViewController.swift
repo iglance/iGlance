53527f51893c10f3454ba1c7e0791c8521847b2f
Dominik H <36657497+D0miH@users.noreply.github.com>
2019-12-18 18:18:57 +0100

    visual effect view now has proper material

--- iGlance/iGlance/PreferenceWindow/MainViews/MainViewViewController.swift
+++ iGlance/iGlance/PreferenceWindow/MainViews/MainViewViewController.swift
@@ -13,8 +13,10 @@ class MainViewViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // add the visual effect view for the background blur
+        // create the visual effect view for the background blur
         let visualEffectView = NSVisualEffectView(frame: self.view.frame)
+        visualEffectView.material = NSVisualEffectView.Material.underWindowBackground
+        // add the visual effect view
         self.view.addSubview(visualEffectView, positioned: NSWindow.OrderingMode.below, relativeTo: nil)
     }
 }
