02c55e2ca7c515f37fa14ecc40f63dfcae06f5c6
Dominik H <36657497+D0miH@users.noreply.github.com>
2020-03-21 19:25:59 +0100

    added function to get remaining battery time and remaining charge

--- iGlance/iGlance/iGlance/SystemInfo/BatteryInfo.swift
+++ iGlance/iGlance/iGlance/SystemInfo/BatteryInfo.swift
@@ -7,12 +7,32 @@
 //
 
 import Foundation
+import IOKit.ps
 import SystemKit
 import CocoaLumberjack
 
 class BatteryInfo {
+    // MARK: -
+    // MARK: Structure Definitions
+    
+    /**
+     * Structure that contains the remaining time of the battery.
+     */
+    struct RemainingBatteryTime {
+        /// The remaining minutes of the battery
+        var minutes: Int
+        /// The remaining hours of the battery
+        var hours: Int
+        /// The error code of the function reading the battery time. Is either `kIOPSTimeRemainingUnknown`, `kIOPSTimeRemainingUnlimited` or `0` if the remaining battery time could be estimated.
+        var errorCode: Double
+    }
+    
+    // MARK: -
+    // MARK: Private Variables
     private var skBattery = SKBattery()
 
+    // MARK: -
+    // MARK: Initializer & Deinitializer
     init() {
         if skBattery.open() != kIOReturnSuccess {
             DDLogError("Opening a connection to the battery failed")
@@ -25,6 +45,9 @@ class BatteryInfo {
         }
     }
 
+    // MARK: -
+    // MARK: Instance Methods
+    
     /**
      * Returns the health of the battery in percent (in the range [0,1]).
      */
@@ -54,5 +77,64 @@ class BatteryInfo {
         let cycleCount = self.skBattery.cycleCount()
         DDLogInfo("Read the cycle count of the battery \(cycleCount)")
         return cycleCount
+    }
+    
+    /**
+     * Returns the estimated time until all power sources are empty
+     */
+    func getRemainingBatteryTime() -> RemainingBatteryTime {
+        // get the remaining time in seconds
+        let remaining: CFTimeInterval = IOPSGetTimeRemainingEstimate()
+        
+        switch remaining {
+        case kIOPSTimeRemainingUnknown:
+            DDLogInfo("The remaining battery time is unknown")
+            // return zero as time
+            return RemainingBatteryTime(minutes: 0, hours: 0, errorCode: kIOPSTimeRemainingUnknown)
+        case kIOPSTimeRemainingUnlimited:
+            DDLogInfo("The system has access to an unlimited power source (AC)")
+            return RemainingBatteryTime(minutes: 0, hours: 0, errorCode: kIOPSTimeRemainingUnlimited)
+        default:
+            DDLogInfo("Remaining battery time is \(remaining) seconds")
+            
+            // calculate the remaining minutes and hours
+            let remainingMinutes = Int(floor(remaining / 60))
+            let hours = remainingMinutes / 60
+            let minutes = remainingMinutes % 60
+            
+            return RemainingBatteryTime(minutes: minutes, hours: hours, errorCode: 0)
+        }
+    }
+    
+    /**
+     * Returns the remaining battery charge in percentage (between 0 and 100). The function returns nil if an error occurred.
+     */
+    func getRemainingCharge() -> Int? {
+        // get the power source handles
+        let psSnapshot = IOPSCopyPowerSourcesInfo().takeRetainedValue()
+        let psHandles = IOPSCopyPowerSourcesList(psSnapshot).takeRetainedValue() as Array
+        
+        for psHandle in psHandles {
+            // get the power source info
+            if let psInfo = IOPSGetPowerSourceDescription(psSnapshot, psHandle).takeRetainedValue() as? [String: AnyObject] {
+                guard let name = psInfo[kIOPSNameKey] as? String else {
+                    DDLogError("Could not read the name of the power source")
+                    return nil
+                }
+                
+                guard let currentCapacity = psInfo[kIOPSCurrentCapacityKey] as? Int else {
+                    DDLogInfo("Failed to read the current capacity of the power source with the name \(name)")
+                    return nil
+                }
+                
+                if name == "InternalBattery-0" {
+                    return currentCapacity
+                }
+            }
+        }
+
+        // if we got to this point in the function something went wrong
+        DDLogError("Could not read the power source info")
+        return nil
     }
 }
