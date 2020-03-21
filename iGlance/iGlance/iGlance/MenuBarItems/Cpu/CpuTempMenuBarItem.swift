3c3a6575cc24c41b592ccee9d67d51043eb1eee8
Dominik H <36657497+D0miH@users.noreply.github.com>
2020-03-21 16:48:50 +0100

    cpu temperature is now rendered on an image

--- iGlance/iGlance/iGlance/MenuBarItems/Cpu/CpuTempMenuBarItem.swift
+++ iGlance/iGlance/iGlance/MenuBarItems/Cpu/CpuTempMenuBarItem.swift
@@ -13,13 +13,49 @@ class CpuTempMenuBarItem: MenuBarItem {
     // MARK: -
     // MARK: Protocol Implementations
     func update() {
-        let temp: Double = AppDelegate.systemInfo.cpu.getCpuTemp()
+        let temp = 200//Int(AppDelegate.systemInfo.cpu.getCpuTemp())
 
         guard let button = self.statusItem.button else {
             DDLogError("Could not retrieve the button of the 'CpuTempMenuBarItem'")
             return
         }
 
-        button.title = String(Int(temp)) + "°"
+        // get the string that is going to be rendered
+        let buttonString = createAttributedTempString(value: temp)
+
+        // create an image for the menu bar item
+        let imageWidth = 30
+        let image = NSImage(size: NSSize(width: imageWidth, height: 18))
+
+        // lock the image to render the string
+        image.lockFocus()
+
+        // render the string
+        buttonString.draw(at: NSPoint(x: image.size.width - buttonString.size().width, y: image.size.height / 2 - buttonString.size().height / 2))
+
+        // unlock the focus of the image
+        image.unlockFocus()
+
+        button.image = image
+    }
+
+    /**
+     * Returns the attributed string of the current cpu temperature that can be rendered on an image.
+     *
+     *  - Parameter value: The given cpu temperature
+     */
+    private func createAttributedTempString(value: Int) -> NSAttributedString {
+        // create the attributed string
+        let string = String(value) + "°"
+        let attribString = NSMutableAttributedString(string: string)
+
+        // define the font
+        let font = NSFont(name: "Apple SD Gothic Neo", size: 14)!
+
+        attribString.addAttribute(.font, value: font, range: NSRange(location: 0, length: string.count))
+        let fontColor = ThemeManager.isDarkTheme() ? NSColor.white : NSColor.black
+        attribString.addAttribute(.foregroundColor, value: fontColor, range: NSRange(location: 0, length: string.count))
+
+        return attribString
     }
 }
